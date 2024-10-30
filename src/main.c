809b67fe26d6c9f11ed769912c4a19a950915c0a
rishabhkr-r111 <rishabhkrr111@gmail.com>
2024-10-31 02:15:44 +0530

    basic stack

--- src/main.c
+++ src/main.c
@@ -19,18 +19,50 @@ const int pgm[] = {
 
 int ip = 0;
 bool running = true;
+int sp = -1;
+int stack[256];
+
 
 int fetch(){
   return pgm[ip];
 }
 
+void eval(int inst){
+  switch (inst)
+  {
+  case HLT: {
+    running = false;
+    break;
+  }
+  case PSH:{
+    stack[++sp] = pgm[++ip];
+    break;
+  }
+  case POP:{
+    int x = stack[sp];
+    printf("poped value %d \n", x);
+    sp--;
+
+    break;
+  }
+  case ADD:{
+    int x = stack[sp--];
+    int y = stack[sp--];
+    stack[++sp] = x + y;
+
+    break;
+  }
+  
+  default:
+    break;
+  }
+}
+
 int main() {
   while(running) {
     int inst = fetch();
-    printf("%d", inst);
-    if(inst == HLT){
-      running = false;
-    }
+    printf("%d \n", inst);
+    eval(inst);
     ip++;
   }
   return 0;
